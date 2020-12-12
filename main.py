c90a54c3993562be7ac8ddec1241857d25a67525
tabishhameed <tabishhameed34@gmail.com>
2020-12-12 16:18:44 +0800

    class with 4 methods

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-ghsjfhsdjjfsdlkf
+class garage:
+    def __init__(self):
+        self,cars = []
+
+    def __len__(self):
+        return len(self.cars)
+
+    def __add__(self, car):
+        print('the method is a work in progress')
