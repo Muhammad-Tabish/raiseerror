d00c2bd4a7c85181fc323fb0473275813ac5db18
tabishhameed <tabishhameed34@gmail.com>
2020-12-12 16:51:00 +0800

    CREATE TYPE ERROR IN ADD_CAR METHOD

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def __repr__(self):
+        return f'<car {self.make} {self.model}>'
+
+
 class garage:
     def __init__(self):
         self.cars = []
@@ -5,9 +14,10 @@ class garage:
     def __len__(self):
         return len(self.cars)
 
-    def add_cars(self, car):
-         raise NotImplementedError['we cant add cars']
+    def add_car(self, car):
+        if not isinstance(car, Car):
+            raise TypeError(f'tried to add a {car.__class__.__name__} to the garage ')
+        self.cars.append(car)
 
 ford = garage()
-ford.add_cars('fiesta')
-print(len(ford))
+ford.add_car('fiesta')
