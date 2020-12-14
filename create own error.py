a9612cc547a18008f59fc10ae156d190cce7d712
tabishhameed <tabishhameed34@gmail.com>
2020-12-14 17:35:54 +0800

    create docstring

--- create own error.py
+++ create own error.py
@@ -1,6 +1,10 @@
-class MyCustomError(TypeError):
+class RuntimeError(TypeError):
+    """
+    EXception raised when a specific error code is needed
+    """
     def __init__(self, message, code):
         super().__init__(f'Error code {code} : {message }')
         self.code = code
 
-raise  MyCustomError('OUCH An error happend', 500)
+err = RuntimeError('An Error happened', 500)
+print(err.__doc__)
