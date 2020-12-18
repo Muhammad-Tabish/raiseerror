77c5be8111cb37a1700af6c586260130b9db43ec
tabishhameed <tabishhameed34@gmail.com>
2020-12-18 16:52:44 +0800

    text app

--- dealing with python errors.py
+++ dealing with python errors.py
@@ -1,32 +1,4 @@
-class Car:
-    def __init__(self, make, model):
-        self.make = make
-        self.model = model
-
-    def __repr__(self):
-        return f'<car {self.make} {self.model}>'
-
-
-class garage:
-    def __init__(self):
-        self.cars = []
-
-    def __len__(self):
-        return len(self.cars)
-
-    def add_car(self, car):
-        if not isinstance(car, Car):
-            raise ValueError(f'tried to add a {car.__class__.__name__} to the garage ')
-        self.cars.append(car)
-
-ford = garage ()
-fiesta = Car('ford', 'fiesta')
-
-try:
-    ford.add_car('fiesta')
-except TypeError:
-     print('your car was not a car')
-except ValueError:
-    print('something weird happend')
-finally:
-    print(f'the garage now has {len(ford)} cars')
+def power_of_two(n):
+    n = input('Please enter a number: ')
+    n_square = n ** 2
+    print(f'the value {n_square
